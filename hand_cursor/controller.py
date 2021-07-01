20003e42a00704be7b88ae39c99c8629c00bed4d
DACUS1995 <studormarian@gmail.com>
2021-07-02 00:57:46 +0300

    relative movement

--- hand_cursor/controller.py
+++ hand_cursor/controller.py
@@ -14,6 +14,7 @@ class Controller():
 		self.cursor = cursor
 		self.is_running = False
 		self.last_action_timestamp = time.time()
+		self._last_open_hand_coordinates = None
 
 	def start(self):
 		self.is_running = True
@@ -44,17 +45,27 @@ class Controller():
 	def _update(self, landmarks, type:HandType):
 		landmark = landmarks.landmark
 		if type == HandType.OPEN_HAND:
-			coordinates = (
-				self.cursor.screen_size[0] * landmark[config.OPEN_HAND_LANDMARK].x,
-				self.cursor.screen_size[1] * landmark[config.OPEN_HAND_LANDMARK].y
-			)
-			self.cursor.set_position(coordinates)
+			if self._last_open_hand_coordinates is not None:
+				coordinates = (
+					self.cursor.screen_size[0] * landmark[config.OPEN_HAND_LANDMARK].x - self._last_open_hand_coordinates[0],
+					self.cursor.screen_size[1] * landmark[config.OPEN_HAND_LANDMARK].y - self._last_open_hand_coordinates[1]
+				)
+				self.cursor.move_rel(coordinates)
 		elif type == HandType.LEFT_CLICK_HAND:
 			self._perform_cursor_action(CursorAction.LEFT_CLICK_ACTION)
 		elif type == HandType.CLOSE_HAND:
 			pass
 		else:
 			raise Exception("Unhandled hand type")
+
+		if type == HandType.OPEN_HAND:
+			self._last_open_hand_coordinates = (
+				self.cursor.screen_size[0] * landmark[config.OPEN_HAND_LANDMARK].x,
+				self.cursor.screen_size[1] * landmark[config.OPEN_HAND_LANDMARK].y
+			)
+		else:
+			self._last_open_hand_coordinates = None
+
 
 	def _perform_cursor_action(self, action:CursorAction):
 		curr_time = time.time()
